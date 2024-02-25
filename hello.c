9c258ae36c00a5290b46782e0d0a5c4787413fa1
Abdul Mateen <ranamateen2004@gmail.com>
2024-02-25 19:13:54 +0500

    First commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//Abdul Mateen
+//Reg no: FA23-BCS-014
+/*This program uses printf statement to draw shapes*/
+int main()
+{
+    printf("\n           *                 ");
+    printf("\n          ***                ");
+    printf("\n         *****                   ");
+    printf("\n          ***                  ");
+    printf("\n           *                 ");
+    return 0;
+
+
+
+
+
+
+
+}
